b4b8bda8f2e1138eafc7e8a6017b7ede37c7d08d
Fitz Elliott <fitz@cos.io>
2023-05-15 16:46:44 -0400

    add charon to INSTALLED_APPS

--- gravyvalet/settings.py
+++ gravyvalet/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'charon.apps.CharonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

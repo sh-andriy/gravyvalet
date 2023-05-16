cbad51ed6a4aff65267f4cb0ff4bebf5d2ed29be
Fitz Elliott <fitz@cos.io>
2023-05-16 13:37:12 -0400

    add simple handler for box

--- charon/views.py
+++ charon/views.py
@@ -1,11 +1,22 @@
+import logging
+
+from django.http import HttpResponse
+
 # from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)
 
 
 def index(request):
     return HttpResponse(
         "Hello, world. Welcome to the continental, rated two stars on tripadvisor."
     )
+
+
+def connect_box(request):
+    logger.info('@@@ got request for connect_box')
+    logger.info('@@@   request ib:({})'.format(request))
+    return HttpResponse("You tried to box, but box we didn't.")

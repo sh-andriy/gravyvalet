543692d8cf6a9eac456853056bf3742b033f4204
Abram Booth <boothaa@gmail.com>
2023-11-29 14:55:37 -0500

    add `fill_garbage` management command
    example usage: `python3 manage.py fill_garbage a b c`
    
    for each label provided, will create a set of connected objects in the
    database for local testing

---
+++ addon_service/management/commands/fill_garbage.py
@@ -0,0 +1,45 @@
+from django.conf import settings
+from django.core.management.base import LabelCommand
+
+from addon_service import models as db
+
+
+class Command(LabelCommand):
+    """add garbage to the database for local/manual testing
+
+    provide one or more labels; will create one set of connected objects for each label
+    """
+
+    def handle_label(self, label, **options):
+        if not settings.DEBUG:
+            raise Exception("must have DEBUG set to eat garbage")
+        _es = db.ExternalService.objects.create(name=f"entity-{label}")
+        _ess = db.ExternalStorageService.objects.create(
+            max_concurrent_downloads=2,
+            max_upload_mb=2,
+            auth_uri=f"http://foo.example/{label}",
+            external_service=_es,
+        )
+        _iu = db.InternalUser.objects.create(
+            user_uri=f"http://osf.example/u{label}",
+        )
+        _ec = db.ExternalCredentials.objects.create()
+        _ea = db.ExternalAccount.objects.create(
+            remote_account_id=label,
+            remote_account_display_name=label,
+            external_service=_es,
+            owner=_iu,
+            credentials=_ec,
+        )
+        _asa = db.AuthorizedStorageAccount.objects.create(
+            external_storage_service=_ess,
+            external_account=_ea,
+        )
+        _ir = db.InternalResource.objects.create(
+            resource_uri=f"http://osf.example/r{label}",
+        )
+        _csa = db.ConfiguredStorageAddon.objects.create(
+            authorized_storage_account=_asa,
+            internal_resource=_ir,
+        )
+        return str(_csa)

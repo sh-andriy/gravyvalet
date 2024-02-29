1124609a5587fd188b66112a5bd8f4f0a1f6b4a4
John Tordoff <>
2024-02-29 12:24:25 -0500

    use syntax check

--- addon_service/management/commands/test_disabled_accounts_subscription.py
+++ addon_service/management/commands/test_disabled_accounts_subscription.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
+
 from addon_service.tasks import listen_for_disabled_users
 
+
 class Command(BaseCommand):
-    help = 'Subscribes to an RPC exchange and listens for messages.'
+    help = "Subscribes to an RPC exchange and listens for messages."
 
     def handle(self, *args, **options):
         listen_for_disabled_users()
--- addon_service/tasks.py
+++ addon_service/tasks.py
@@ -1,13 +1,19 @@
 import Celery
+from kombu import (
+    Consumer,
+    Exchange,
+    Queue,
+)
 
-from kombu import Queue, Exchange, Consumer
-OSF_DEFAULT_QUEUE = 'celery'
+
+OSF_DEFAULT_QUEUE = "celery"
 from addon_service.models import UserReference
 from app.settings import OSF_BROKER_URL
 
+
 def process_disabled_user_message(body, message):
     print(f"Received message: {body}")
-    user_uri = body['user_uri']
+    user_uri = body["user_uri"]
     UserReference.objects.get(user_uri=user_uri).delete()
     message.ack()
 
@@ -15,15 +21,17 @@ def process_disabled_user_message(body, message):
 def listen_for_osf_signals():
     with Celery(broker=OSF_BROKER_URL).connection() as connection:
         with Consumer(
-                connection,
-                queues=Queue(
-                    name=OSF_DEFAULT_QUEUE,  # TODO: Any plans here?
-                    exchange=Exchange(OSF_DEFAULT_QUEUE),  # TODO: Define exchange(s) for merged/disabled
-                ),
-                callbacks=[  # TODO: Multiple callbacks or multiple queues?
-                    process_disabled_user_message
-                ],
-                accept=['json']
+            connection,
+            queues=Queue(
+                name=OSF_DEFAULT_QUEUE,  # TODO: Any plans here?
+                exchange=Exchange(
+                    OSF_DEFAULT_QUEUE
+                ),  # TODO: Define exchange(s) for merged/disabled
+            ),
+            callbacks=[  # TODO: Multiple callbacks or multiple queues?
+                process_disabled_user_message
+            ],
+            accept=["json"],
         ):
             while True:
                 connection.drain_events()

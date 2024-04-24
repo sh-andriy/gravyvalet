9645cb6207e593f02c49ca3604462ef2951104e4
Jon Walz <jonwalz@V0FK3G2JY7.localdomain>
2024-04-24 14:48:40 -0400

    Remove unnecessary subclassing

--- addon_service/common/permissions.py
+++ addon_service/common/permissions.py
@@ -12,7 +12,7 @@ class IsAuthenticated(permissions.BasePermission):
         return request.session.get("user_reference_uri") is not None
 
 
-class SessionUserIsOwner(IsAuthenticated):
+class SessionUserIsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         session_user_uri = request.session.get("user_reference_uri")
         if session_user_uri:

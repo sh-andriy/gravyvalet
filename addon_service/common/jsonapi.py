1d45a6f472627513cb5a7164d6e6324f2879b572
Jon Walz <jonwalz@V0FK3G2JY7.localdomain>
2024-04-24 14:47:06 -0400

    Remove `extract_filter_expressions` from jsonapi.py

--- addon_service/common/jsonapi.py
+++ addon_service/common/jsonapi.py
@@ -106,22 +106,3 @@ def group_query_params_by_family(
             parsed_query_param
         )
     return grouped_query_params
-
-
-def extract_filter_expressions(query_dict) -> dict[str, str]:
-    """Extract the "filter" family of expressions from the query dict and format them for use.
-
-    Since no formal JSON:API scheme exists for complex filter operations, we will
-    Assumes that nested brackets are used to represent the equivalent of a `__` in Django,
-    i.e. a nested lookup or a named, non-equality comparison operation.
-
-    For example:
-    addons.osf.io/v1/user_references/{key}/authorized_storage_accounts/?filter=[created][lt]=YYYYYMMDD
-    would return only the authorized storage accounts for a given user that are older than the provided date
-
-    Similarly
-    /v1/resource_references/{key}/configured_storage_addons/?filter[base_account][external_strorage_service][name]=GoogleDrive
-    would return just the Google Drive addons for a given resource.
-    """
-    filter_params = group_query_params_by_family(query_dict.lists()).get("filter", [])
-    return {"__".join(param.members): ",".join(param.values) for param in filter_params}

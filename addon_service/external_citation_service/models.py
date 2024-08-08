b3722f607e1edd1fd4c6e21323da3c6b3e4cc92a
Vlad0n20 <vlad.onischuk1234@gmail.com>
2024-08-08 12:50:46 +0300

    Remove max_concurrent_downloads and max_upload_mb field from ExternalCitationService

--- addon_service/external_citation_service/models.py
+++ addon_service/external_citation_service/models.py
@@ -1,17 +1,13 @@
 from django.db import models
 
 from addon_service.abstract.external_storage.models import ExternalService
-from addon_service.common.validators import validate_storage_imp_number
 
 
 class ExternalCitationService(ExternalService):
     int_addon_imp = models.IntegerField(
         null=False,
-        validators=[validate_storage_imp_number],
         verbose_name="Addon implementation",
     )
-    max_concurrent_downloads = models.IntegerField(null=False)
-    max_upload_mb = models.IntegerField(null=False)
     wb_key = models.CharField(null=False, blank=True, default="")
     oauth1_client_config = models.ForeignKey(
         "addon_service.OAuth1ClientConfig",

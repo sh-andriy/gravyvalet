bf41c5c86d9e08016413c3c0a058f5e9fa354de7
Yuhuai Liu <yuhuai@cos.io>
2024-12-10 23:23:11 -0500

    fix moar tests

--- addon_service/tests/test_by_type/test_user_reference.py
+++ addon_service/tests/test_by_type/test_user_reference.py
@@ -65,6 +65,7 @@ class TestUserReferenceAPI(APITestCase):
                 _content["data"]["relationships"].keys(),
                 {
                     "authorized_storage_accounts",
+                    "authorized_citation_accounts",
                     "configured_resources",
                 },
             )
@@ -198,6 +199,7 @@ class TestUserReferenceViewSet(TestCase):
             set(_content["data"]["relationships"].keys()),
             {
                 "authorized_storage_accounts",
+                "authorized_citation_accounts",
                 "configured_resources",
             },
         )

7bb0af01bbb8d05380bf91f1caaceb60c181f066
Oleh Paduchak <opaduchak@exoft.net>
2024-12-18 17:45:06 +0200

    fixed supported_features for citation external services

--- addon_service/external_service/citation/serializers.py
+++ addon_service/external_service/citation/serializers.py
@@ -3,7 +3,9 @@ from rest_framework_json_api.utils import get_resource_type_from_model
 
 from addon_service.addon_imp.models import AddonImpModel
 from addon_service.common import view_names
+from addon_service.common.enum_serializers import EnumNameMultipleChoiceField
 from addon_service.common.serializer_fields import DataclassRelatedDataField
+from addon_service.external_service.citation.models import CitationSupportedFeatures
 from addon_service.external_service.serializers import ExternalServiceSerializer
 from addon_service.models import ExternalCitationService
 
@@ -21,6 +23,9 @@ class ExternalCitationServiceSerializer(ExternalServiceSerializer):
         dataclass_model=AddonImpModel,
         related_link_view_name=view_names.related_view(RESOURCE_TYPE),
     )
+    supported_features = EnumNameMultipleChoiceField(
+        enum_cls=CitationSupportedFeatures, read_only=True
+    )
 
     class Meta:
         model = ExternalCitationService
@@ -34,4 +39,5 @@ class ExternalCitationServiceSerializer(ExternalServiceSerializer):
             "configurable_api_root",
             "wb_key",
             "external_service_name",
+            "supported_features",
         ]
